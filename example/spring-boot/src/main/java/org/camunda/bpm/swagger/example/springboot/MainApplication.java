71a9d57bdbf7e6f3dca8c86b2c26b7b81ab5174e
Jan Galinski <jan.galinski@holisticon.de>
2018-02-08 21:57:17 +0100

    prepare redirect

--- example/spring-boot/src/main/java/org/camunda/bpm/swagger/example/springboot/MainApplication.java
+++ example/spring-boot/src/main/java/org/camunda/bpm/swagger/example/springboot/MainApplication.java
@@ -5,7 +5,10 @@ import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableProcessApplication
@@ -13,6 +16,17 @@ public class MainApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MainApplication.class, args);
+  }
+
+  //@Bean
+  WebMvcConfigurerAdapter webMvcConfigurerAdapter() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/swagger", "/webjars/swagger-ui/3.1.4");
+        super.addViewControllers(registry);
+      }
+    };
   }
 
   @EventListener
